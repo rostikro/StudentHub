09bfc137f1e6cc9144a9c14322c8cd00b3399bb2
IvanAxe-Dev <101930216+IvanAxe-Dev@users.noreply.github.com>
2023-10-16 17:33:55 +0300

    Added JWT token generation (#1)
    * Added JWT token generation
    
    * Cosmetic appsettings changes

---
+++ SoftServeProject3.Api/Configurations/KeyGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+public class KeyGenerator
+{
+    public static string GenerateRandomKey(int length)
+    {
+        using (var cryptoProvider = new RNGCryptoServiceProvider())
+        {
+            byte[] data = new byte[length];
+            cryptoProvider.GetBytes(data);
+            return Convert.ToBase64String(data);
+        }
+    }
+}
