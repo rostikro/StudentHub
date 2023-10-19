81d36fc62a2435f378fa682cd2b235f4ec9803ea
Rostik <Rostik@DESKTOP-I9ORA34>
2023-10-19 16:44:33 +0300

    Account confirmation system added.

---
+++ SoftServeProject3.Api/Utils/RandomGenerator.cs
@@ -0,0 +1,21 @@
+﻿namespace SoftServeProject3.Api.Utils;
+
+public static class RandomGenerator
+{
+     
+    
+    public static string GenerateRandomCode()
+    {
+        var chars = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"; // 62 chars
+
+        Random rnd = new Random();
+
+        string result = "";
+        for (int i = 0; i < 6; i++)
+        {
+            result += chars[rnd.Next(chars.Length)];
+        }
+
+        return result;
+    }
+}
