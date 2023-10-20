f580824b2b3d277f5c8744f34473d6ba540c9f2b
Rostik <67545093+rostikro@users.noreply.github.com>
2023-10-20 14:09:17 +0300

    Account confirmation system added. (#8)
    * Account confirmation system added.
    
    * Account confirmation system added.
    
    ---------
    
    Co-authored-by: Rostik <Rostik@DESKTOP-I9ORA34>

--- SoftServeProject3.Api/Interfaces/IUserRepository.cs
+++ SoftServeProject3.Api/Interfaces/IUserRepository.cs
@@ -4,6 +4,8 @@ namespace SoftServeProject3.Api.Interfaces
 {
     public interface IUserRepository
     {
+        Task UpdateUserAsync(string email);
+        Task<bool> IsUserExistsAsync(string email);
         User GetByEmail(string email);
         void Register(User user);
 
