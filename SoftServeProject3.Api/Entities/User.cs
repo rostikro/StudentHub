fa9283e96eee89d57a1d0caa489baaa55d33a0ef
Roman <kozakevycr2@gmail.com>
2023-11-03 14:08:49 +0200

    коміт

--- SoftServeProject3.Api/Entities/User.cs
+++ SoftServeProject3.Api/Entities/User.cs
@@ -20,6 +20,23 @@ namespace SoftServeProject3.Api.Entities
         
         [BsonElement("isEmailConfirmed")]
         public bool IsEmailConfirmed { get; set; }
+        [BsonElement("photoUrl")]
+        public string PhotoUrl { get; set; }
+
+        [BsonElement("faculty")]
+        public string Faculty { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; }
+
+        [BsonElement("desription")]
+        public string Description { get; set; }
+
+        [BsonElement("subjects")]
+        public List<string> Subjects { get; set; }
+
+        [BsonElement("social")]
+        public Dictionary<string, string> Social { get; set; }
         [BsonElement("schedule")]
         public Dictionary<string, List<TimeRange>> Schedule { get; set; }
     }
