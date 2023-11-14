1a2ac9b14bc328928a55275fd5016eb671439549
Yaroslav Rosnovskiy <rosn.yaroslav@gmail.com>
2023-11-14 10:33:11 +0200

    Прибрав вже лишні поля

--- SoftServeProject3.Core/DTOs/UserModel.cs
+++ SoftServeProject3.Core/DTOs/UserModel.cs
@@ -68,16 +68,6 @@ namespace SoftServeProject3.Core.DTOs
         [JsonProperty("schedule")]
         [BsonElement("schedule")]
         public Dictionary<string, List<TimeRange>> Schedule { get; set; } //= new ();
-
-        [JsonProperty("friends")]
-        [BsonElement("friends")]
-        public List<ObjectId> Friends { get; set; }
-        [JsonProperty("incomingFriendRequests")]
-        [BsonElement("incomingFriendRequests")]
-        public List<ObjectId> InFriends { get; set; }
-        [JsonProperty("outgoingFriendRequests")]
-        [BsonElement("outgoingFriendRequests")]
-        public List<ObjectId> OutFriends { get; set; }
     }
     public class ScheduleDay
     {
