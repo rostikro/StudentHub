050986ab11c228c898faceb5eb4be10bc4a9474f
Rosikss <76153577+Rosikss@users.noreply.github.com>
2023-11-25 17:04:09 +0200

    Додав пагінацію (#64)

---
+++ SoftServeProject3.Core/DTOs/UserSearchResultModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftServeProject3.Core.DTOs
+{
+    public class UserSearchResultModel
+    {
+        public int TotalCount { get; set; }
+        public UserListModel[] Users { get; set; }
+    }
+
+}
